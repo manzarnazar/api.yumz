f5fda708b030716c3f5a0df2eeaf0e74d961e54f
manzarnazar <manzarnazar@gmail.com>
2025-01-12 15:02:45 +0500

    fixed_commission_per_Order

--- app/Http/Controllers/API/v1/Rest/DeliveryZoneController.php
+++ app/Http/Controllers/API/v1/Rest/DeliveryZoneController.php
@@ -86,47 +86,39 @@ class DeliveryZoneController extends RestBaseController
 	 * @param CheckDistanceRequest $request
 	 * @return JsonResponse
 	 */
-	// public function checkDistance(CheckDistanceRequest $request): JsonResponse
-	// {
-	// 	$shops = Shop::with('deliveryZone:id,shop_id,address')
-	// 		->where([
-	// 			['open', 1],
-	// 			['status', 'approved'],
-	// 		])
-	// 		->whereHas('deliveryZone')
-	// 		->select(['id', 'open', 'status'])
-	// 		->get();
-
-	// 	foreach ($shops as $shop) {
-
-	// 		/** @var Shop $shop */
-	// 		$deliveryZone = $shop->deliveryZone;
-
-	// 		if (!is_array($deliveryZone?->address) || count($deliveryZone?->address ?? []) === 0) {
-	// 			continue;
-	// 		}
-
-	// 		$check = Utility::pointInPolygon($request->input('address'), $shop->deliveryZone->address);
-
-	// 		if ($check) {
-	// 			return $this->successResponse('success', 'success');
-	// 		}
-
-	// 	}
-
-	// 	return $this->onErrorResponse([
-	// 		'code'    => ResponseError::ERROR_400,
-	// 		'message' => __('errors.' . ResponseError::ERROR_400, locale: $this->language)
-	// 	]);
-	// }
-
 	public function checkDistance(CheckDistanceRequest $request): JsonResponse
-{
-    // Return only the address data from the request
-    return response()->json([
-        'zipcode' => $request->input('test'),
-    ]);
-}
+	{
+		$shops = Shop::with('deliveryZone:id,shop_id,address')
+			->where([
+				['open', 1],
+				['status', 'approved'],
+			])
+			->whereHas('deliveryZone')
+			->select(['id', 'open', 'status'])
+			->get();
+
+		foreach ($shops as $shop) {
+
+			/** @var Shop $shop */
+			$deliveryZone = $shop->deliveryZone;
+
+			if (!is_array($deliveryZone?->address) || count($deliveryZone?->address ?? []) === 0) {
+				continue;
+			}
+
+			$check = Utility::pointInPolygon($request->input('address'), $shop->deliveryZone->address);
+
+			if ($check) {
+				return $this->successResponse('success', 'success');
+			}
+
+		}
+
+		return $this->onErrorResponse([
+			'code'    => ResponseError::ERROR_400,
+			'message' => __('errors.' . ResponseError::ERROR_400, locale: $this->language)
+		]);
+	}
 
 	/**
 	 * @param int $id
