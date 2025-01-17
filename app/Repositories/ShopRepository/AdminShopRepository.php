dfd1c70a188b75c17778842b539e04ece148dee1
manzarnazar <manzarnazar@gmail.com>
2025-01-17 12:39:58 +0500

    created relationship

--- app/Repositories/ShopRepository/AdminShopRepository.php
+++ app/Repositories/ShopRepository/AdminShopRepository.php
@@ -74,16 +74,16 @@ class AdminShopRepository extends CoreRepository
             ])
             ->select([
                 'id',
-                // 'uuid',
-                // 'background_img',
-                // 'logo_img',
-                // 'open',
-                // 'visibility',
-                // 'verify',
-                // 'tax',
-                // 'status',
-                // 'user_id',
-                // 'deleted_at',
+                'uuid',
+                'background_img',
+                'logo_img',
+                'open',
+                'visibility',
+                'verify',
+                'tax',
+                'status',
+                'user_id',
+                'deleted_at',
             ])
             ->orderBy(data_get($filter, 'column', 'id'), data_get($filter, 'sort', 'desc'))
             ->paginate(data_get($filter, 'perPage', 10));
