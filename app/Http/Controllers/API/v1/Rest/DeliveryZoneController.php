eaf33186208c93318b8ef1f37e906642a2464e20
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-01-22 19:11:01 +0500

    created relationship

--- app/Http/Controllers/API/v1/Rest/DeliveryZoneController.php
+++ app/Http/Controllers/API/v1/Rest/DeliveryZoneController.php
@@ -9,7 +9,6 @@ use App\Http\Requests\DeliveryZone\DistanceRequest;
 use App\Http\Requests\FilterParamsRequest;
 use App\Models\DeliveryZone;
 use App\Models\Shop;
-use App\Models\ShopDeliveryZipcode;
 use App\Traits\SetCurrency;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -89,20 +88,32 @@ class DeliveryZoneController extends RestBaseController
 	 */
 	public function checkDistance(CheckDistanceRequest $request): JsonResponse
 	{
-		$requestedZipCode = $request->input('address.zip_code');
-		$requestedCity = $request->input('address.city');
+		$shops = Shop::with('deliveryZone:id,shop_id,address')
+			->where([
+				['open', 1],
+				['status', 'approved'],
+			])
+			->whereHas('deliveryZone')
+			->select(['id', 'open', 'status'])
+			->get();
 
-		// Check if the requested zip code exists in the shop_delivery_zipcodes table
-		$locationExists = ShopDeliveryZipcode::where('zip_code', $requestedZipCode)
-        ->orWhere('city', $requestedCity)
-        ->exists();
-	
-		// If the zip code exists, return success response
-		if ($locationExists) {
-			return $this->successResponse('success', 'Zip code matched successfully');
+		foreach ($shops as $shop) {
+
+			/** @var Shop $shop */
+			$deliveryZone = $shop->deliveryZone;
+
+			if (!is_array($deliveryZone?->address) || count($deliveryZone?->address ?? []) === 0) {
+				continue;
+			}
+
+			$check = Utility::pointInPolygon($request->input('address'), $shop->deliveryZone->address);
+
+			if ($check) {
+				return $this->successResponse('success', 'success');
+			}
+
 		}
-	
-		// If the zip code does not exist, return error response
+
 		return $this->onErrorResponse([
 			'code'    => ResponseError::ERROR_400,
 			'message' => __('errors.' . ResponseError::ERROR_400, locale: $this->language)
