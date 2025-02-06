3d8b1bc85db5f5df69b47c36a53472e63656cdde
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-06 13:10:28 +0500

    test

--- app/Http/Controllers/API/v1/Dashboard/Admin/ShopController.php
+++ app/Http/Controllers/API/v1/Dashboard/Admin/ShopController.php
@@ -169,6 +169,9 @@ class ShopController extends AdminBaseController
      */
     public function update(StoreRequest $request, string $uuid): JsonResponse
     {
+
+        \Log::info('Shop Update Request - Open Status:', ['open' => $request->input('open')]);
+
         $shop = Shop::where(['user_id' => $request->input('user_id'), 'uuid' => $uuid])->first();
 
         $seller = User::find($request->input('user_id'));
