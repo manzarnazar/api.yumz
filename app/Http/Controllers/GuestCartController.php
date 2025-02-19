b2c9d32a4a14f71bb50324a23fbae686dddd27f4
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-19 13:52:42 +0500

    fixed

--- app/Http/Controllers/GuestCartController.php
+++ app/Http/Controllers/GuestCartController.php
@@ -14,7 +14,7 @@ class GuestCartController extends Controller
         // Validate incoming request
         $request->validate([
             'guest_id' => 'required|exists:guest_users,id',  // Ensure the guest ID is valid
-            'cart_items' => 'required|array',  // Array of cart items
+            // 'cart_items' => 'required|array',  // Array of cart items
             'shop_id' => 'required|exists:shops,id',  // Ensure the shop ID is valid
             'currency_id' => 'required|exists:currencies,id',  // Ensure currency ID is valid
         ]);
